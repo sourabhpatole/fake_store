bbb35887adb0f7f015d465322a8f4bfc702af009
sourabhpatole <sourabhptole@outlook.com>
2022-05-19 23:47:03 +0530

    code cleanup and arrange

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -1,26 +1,37 @@
-import {
-  Box,
-  Button,
-  Heading,
-  Input,
-  SimpleGrid,
-  Spacer,
-  Stack,
-} from "@chakra-ui/react";
-import React, { useRef } from "react";
+import { Box, GridItem, Input, SimpleGrid } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import { StoreItem } from "./StoreItem";
-const Store = ({ items, onItemAdd }) => {
-  const itemNameRef = useRef();
-  const itemPriceRef = useRef();
+const Store = ({ items }) => {
+  const [filterItems, setFilterItems] = useState(items);
+  useEffect(() => {
+    setFilterItems(items);
+  }, [items]);
+
   return (
-    <Box p={5}>
+    <Box p={4}>
       <Header title="The Shopping Store" />
-      <SimpleGrid columns={4} spacing={5}>
-        {items.map((item) => {
-          return <StoreItem title={item.title} price={item.price} />;
-        })}
-      </SimpleGrid>
+      <Box p={2}>
+        <Input
+          onChange={(e) => {
+            let f = items.filter((item) =>
+              item.title.toLowerCase().includes(e.target.value.toLowerCase())
+            );
+            setFilterItems(f);
+          }}
+          placeholder="Search Item"
+          mt={4}
+        />
+        <SimpleGrid columns={3} spacing={5} mt={2} p={6} backgroundColor="pink">
+          {filterItems.map((item) => {
+            return (
+              <GridItem backgroundColor="white" borderRadius="lg">
+                <StoreItem {...item} />
+              </GridItem>
+            );
+          })}
+        </SimpleGrid>
+      </Box>
     </Box>
   );
 };
--- src/components/StoreItem.jsx
+++ src/components/StoreItem.jsx
@@ -1,14 +1,17 @@
-import { Box, Flex, Heading, Spacer, Tag } from "@chakra-ui/react";
+import { Box, Center, Heading, Image, Spacer, Tag } from "@chakra-ui/react";
 import React from "react";
 
-export const StoreItem = ({ title, price }) => {
+export const StoreItem = ({ title, price, image }) => {
   return (
-    <Box p={4} borderRadius="lg" borderWidth="2px">
+    <Box p={1} borderRadius="lg" mb={4}>
+      <Center>
+        <Image src={image} h="300px" w="240px" m={3} />
+      </Center>
       <Heading noOfLines={3} size="sm" fontWeight="500">
         {title}
       </Heading>
       <Spacer />
-      <Tag mt="5px">₹.{price}</Tag>
+      <Tag mt={5}>₹.{price}</Tag>
     </Box>
   );
 };
